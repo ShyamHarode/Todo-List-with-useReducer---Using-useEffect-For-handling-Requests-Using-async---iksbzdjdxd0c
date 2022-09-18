a32f6bd96e876c37915713845ffa8777fdd1f93a
connectwithabhi23 <107621978+connectwithabhi23@users.noreply.github.com>
2022-09-18 16:37:10 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,5 +1,22 @@
 const todoReducer = (state, action) => {
     switch (action.type) {
+            case "ADD": {
+      const newTodo = {
+        id: Date.now(),
+        heading: action.text,
+      };
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+      };
+    }
+
+    case "DELETE": {
+      const filtered = state.todos.filter((ele) => ele.id !== action.id);
+
+      return { ...state, todos: [...filtered] };
+    }
 
     }
 }
