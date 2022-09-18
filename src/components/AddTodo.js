fbfd1aecdf4c8d3290f32fb4ba5f613c4f06f627
connectwithabhi23 <107621978+connectwithabhi23@users.noreply.github.com>
2022-09-18 16:32:24 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,27 @@
 import React from "react"
+import { useState } from "react";
 
 const AddTodo = ({ dispatch }) => {
+     const [textInput, setTextInput] = useState("");
 
 
     return (
-        <></>
+        <>
+         <form id="todo-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "ADD", text: textInput });
+          setTextInput("");
+        }}
+      >
+        <input id="todo-input"
+          type={"text"}
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+        ></input>
+        <button type={"submit"}>add</button>
+      </form>
+        </>
     )
 }
 
