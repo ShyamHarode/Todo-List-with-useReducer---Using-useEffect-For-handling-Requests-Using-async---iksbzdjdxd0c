1fb9aa5c5eec20edca41ed8513c14fe3bee91bfb
connectwithabhi23 <107621978+connectwithabhi23@users.noreply.github.com>
2022-09-18 16:35:47 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,20 @@
 import React from 'react';
-const Todo = () => {
+const Todo = ({ dispatch, todo }) => {
     return (
-        <></>
+        <div className="todo">
+      {/* {todos.map((ele) => (
+        <h1 key={ele.id}>{ele.heading}</h1>
+      ))} */}
+      <div className="todo-title">
+        {todo.heading}
+        <button
+          className="todo-delete"
+          onClick={() => dispatch({ type: "DELETE", id: todo.id })}
+        >
+          delete
+        </button>
+      </div>
+    </div>
     )
 }
 
